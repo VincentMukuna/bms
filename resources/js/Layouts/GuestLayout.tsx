c52a3ac18eb8b95b87ba9805555ad18d923e271b
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-31 03:56:31 +0300

    [feat] welcome page

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center">
+        <div className="min-h-screen flex flex-col justify-center items-center dark:bg-black/[0.96] antialiased dark:bg-grid-white/[0.02] bg-grid-gray-700/[0.03] relative overflow-hidden">
             {children}
         </div>
     );
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,12 +1,45 @@
-import { Link, Head } from '@inertiajs/react';
+import {  Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
-
+import Guest from "@/Layouts/GuestLayout";
+import {Button} from "@/components/ui/button";
+import {Spotlight} from "@/components/Spotlight";
+import TypewriterEffect, {TypewriterEffectSmooth} from "@/components/typewriter-effect";
 export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
 
-    return (
-        <>
-            <Head title="Welcome" />
-            <p>Billboard Management System</p>
-        </>
-    );
+    const words = [
+        {
+            text: "Manage",
+        },
+        {
+            text: "campaigns",
+        },
+        {
+            text: "with",
+        },
+        {
+            text: "AdvertEase.",
+            className: "text-blue-500 dark:text-blue-500",
+        },
+    ];
+
+    return (<Guest>
+        <Head title="Welcome"/>
+        <Spotlight
+            className="-top-40 left-0 md:left-60 md:-top-40"
+            fill="white"
+        />
+        <div className={" max-w-xl flex flex-col  p-4 text-center  items-center "}>
+            <TypewriterEffectSmooth  words={words} className={'text-4xl'} />
+            <p className={'text-sm sm:text-lg dark:text-muted-foreground text-accent-foreground'}>
+                Take control of your billboard campaigns with our advanced management system.
+                Our platform empowers you to optimize every aspect of your outdoor advertising strategy.
+            </p>
+            <div className={'flex gap-4 mt-4'}>
+                <Button size={'lg'} className={'mt-4'} variant={'secondary'}  >Try it</Button>
+                <Button size={'lg'} className={'mt-4 '}  >Sign Up</Button>
+            </div>
+
+        </div>
+
+    </Guest>);
 }
